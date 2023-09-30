ffd1324c40958b565e25f6ff79ba7532bbc799f0
Hidenori Miyoshi <miyohide@gmail.com>
2023-09-30 21:42:46 +0900

    run flyway migrate command

--- src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
+++ src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
@@ -5,24 +5,38 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.github.miyohide.aws_flyway_lambda.MyLambda.Input;
 import com.github.miyohide.aws_flyway_lambda.MyLambda.Output;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 
 public class MyLambda implements RequestHandler<Input, Output> {
   @Override
   public Output handleRequest(Input input, Context context) {
     final Output out = new Output();
     out.in = input;
-    out.fullName = input.firstName + "_" + input.lastName;
+
+    Flyway flyway = Flyway.configure()
+            .dataSource(input.jdbcURL, input.userName, input.password).load();
+    MigrateResult result = flyway.migrate();
+
+    out.operation = result.getOperation();
 
     return out;
   }
 
+  public String migrateContents(Input input) {
+    return "";
+  }
+
   public static class Input {
-    public String firstName;
-    public String lastName;
+    public String bucket;
+    public String key;
+    public String jdbcURL;
+    public String userName;
+    public String password;
   }
 
   public static class Output {
     public Input in;
-    public String fullName;
+    public String operation;
   }
 }
