739983e3d2ee11c357768d68dd0ac0733e57aead
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-05 18:23:11 +0900

    add bucketName field

--- src/main/java/com/github/miyohide/aws_flyway_lambda/Input.java
+++ src/main/java/com/github/miyohide/aws_flyway_lambda/Input.java
@@ -4,14 +4,16 @@ public class Input {
   private String jdbcURL;
   private String userName;
   private String password;
+  private String bucketName;
 
   public Input() {
   }
 
-  public Input(String jdbcURL, String userName, String password) {
+  public Input(String jdbcURL, String userName, String password, String bucketName) {
     this.jdbcURL = jdbcURL;
     this.userName = userName;
     this.password = password;
+    this.bucketName = bucketName;
   }
 
   public void setJdbcURL(String jdbcURL) {
@@ -26,6 +28,10 @@ public class Input {
     this.password = password;
   }
 
+  public void setBucketName(String bucketName) {
+    this.bucketName = bucketName;
+  }
+
   public String getJdbcURL() {
     return jdbcURL;
   }
@@ -38,12 +44,17 @@ public class Input {
     return password;
   }
 
+  public String getBucketName() {
+    return bucketName;
+  }
+
   @Override
   public String toString() {
     return "Input{" +
             "jdbcURL='" + jdbcURL + '\'' +
             ", userName='" + userName + '\'' +
             ", password='" + password + '\'' +
+            ", bucketName='" + bucketName + '\'' +
             '}';
   }
 }
