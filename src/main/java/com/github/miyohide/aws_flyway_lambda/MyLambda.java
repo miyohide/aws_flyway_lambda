f3499f21d3ae54612f07323f612aa5a63395fb6f
Hidenori Miyoshi <miyohide@gmail.com>
2024-03-25 20:40:23 +0900

    PostgreSQLに接続する処理を追加

--- src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
+++ src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
@@ -7,6 +7,10 @@ import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyLambda implements RequestHandler<Input, String> {
   private final S3Client s3Client = S3Client.builder().region(Region.AP_NORTHEAST_1).build();
 
@@ -14,12 +18,20 @@ public class MyLambda implements RequestHandler<Input, String> {
   public String handleRequest(Input input, Context context) {
     LambdaLogger logger = context.getLogger();
     logger.log("Handler Start", LogLevel.INFO);
+    // S3の操作
     S3Utils s3Utils = new S3Utils(s3Client);
     logger.log("S3Utils#copyFromS3bucket() start", LogLevel.DEBUG);
     s3Utils.copyFromS3bucket(input.getBucketName());
-    // TODO /tmp 以下のファイル一覧を出力させたい
-    logger.log("S3Utils#deleteTmp() start", LogLevel.DEBUG);
-    s3Utils.deleteTmp();
+
+    // JDBCを使ってRDS（PostgreSQL）に接続する
+    logger.log("Connect to the database", LogLevel.DEBUG);
+    try (
+      Connection c = DriverManager.getConnection(input.getJdbcURL(), input.getUserName(), input.getPassword()))
+    {
+      logger.log(c.getCatalog(), LogLevel.INFO);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
     return "success";
   }
 }
