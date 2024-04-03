ba702d667ce85ad3bfd24b37226cacd3295bad04
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-03 21:13:00 +0900

    不要なimport文の削除

--- src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
+++ src/main/java/com/github/miyohide/aws_flyway_lambda/MyLambda.java
@@ -11,7 +11,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.flywaydb.core.Flyway;
 
